a13be1193ac8cfc477c8d3456d15ed97f795fb4d
Ashish Chaudhari <ashishchaudharistudy1857@gmail.com>
2023-08-30 11:38:31 +0530

    node routing

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const http= require("http");
  const  server=http.createServer((req,res)=>{
-    console.log(req)
+    console.log(req.url)
+    if(req.url==='/'){
+     res.write("<html>")
+     res.write("<head> <title> input form</title></head>")
+     res.write("<body> <h1>ashish</h1> <form  action='/msg' method='POST'><input type='text' name='msg' /></form></body>")
+     res.write("</html>")
+     return  res.end()
+    }else if(req.url==="/home"){
+        res.setHeader("content-Type" ,'text/html')
+        res.write("<html>")
+        res.write("<head> <title> welcome  to node js</title></head>")
+        res.write("<body> <h1> welcome  home</h1></body>")
+        res.write("</html>")  
+
+        return  res.end()
+    }
+     res.setHeader("content-Type" ,'text/html')
+     res.write("<html>")
+     res.write("<head> <title> welcome  to node js</title></head>")
+     res.write("<body> <h1> welcome  to node js</h1></body>")
+     res.write("</html>")
 })
 
 server.listen(3000, ()=>{
